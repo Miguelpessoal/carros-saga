afc739c2ab897f5858b821a3dd53f3a427bb650d
Miguel Antonio <miguelpessoal12@gmail.com>
2021-11-09 11:35:40 -0300

    Upando o Projeto

---
+++ resources/views/Company/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Home Page')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Bem-vindo(a)! Vamos gerenciar suas empresas?</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">You are logged in!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'CrudCarros')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Painel de Controle</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">Você está logado!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
